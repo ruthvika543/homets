6b06a526602f118f27bd83dbb9876a5d83c8f84f
ruthvika543 <72019542+ruthvika543@users.noreply.github.com>
2025-12-01 17:09:47 -0500

    Update observablehq.config.js

--- observablehq.config.js
+++ observablehq.config.js
@@ -35,4 +35,5 @@ export default {
   // typographer: false, // smart quotes and other typographic improvements
   // preserveExtension: false, // drop .html from URLs
   // preserveIndex: false, // drop /index from URLs
+  staticDirs: ["public"], 
 };
